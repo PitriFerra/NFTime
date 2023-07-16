1f5cf84c555f685197452a98450838925d135265
PitriFerra <pitriferra@gmail.com>
2023-07-16 11:09:08 +0200

    Big improvements for the dapp
    Interaction with the firebase db reached a stable version. Now the dapp displays the watches in a grid and the user has only to select the watch to mint

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { connectWallet, getCurrentWalletConnected, mintNFT } from "../utils/interact.js";
 import { db } from '../firebase.js'; // Import Firestore database
 import { collection, getDocs } from "firebase/firestore";
-import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
@@ -12,12 +11,10 @@ const Minter = (props) => {
   //State variables ----------------------------------
   const [walletAddress, setWallet] = useState("");
   const [status, setStatus] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [hash, setHash] = useState("");
   const [recipient, setRecipient] = useState("");
 	const [info, setInfo] = useState([]);
   const [filteredInfo, setFilteredInfo] = useState([]); // Filtered watches
+  const [selectedWatch, setSelectedWatch] = useState(null);
   // -------------------------------------------------
 
   // Fetch the required data using the get() method
@@ -76,8 +73,12 @@ const Minter = (props) => {
   };
 
   const onMintPressed = async () => {
-    const { status } = await mintNFT(hash, name, description, recipient);
-    setStatus(status);
+    if (selectedWatch) {
+      const { status } = await mintNFT(recipient, selectedWatch);
+      setStatus(status);
+    } else {
+      setStatus("Please select an item from the list.");
+    }
   };
 
   const handleFilterChange = (event) => {
@@ -86,6 +87,10 @@ const Minter = (props) => {
       watch.model.toLowerCase().includes(value) || watch.brand.toLowerCase().includes(value)
     );
     setFilteredInfo(filteredData);
+  };
+
+  const handleCardClick = (watch) => {
+    setSelectedWatch(watch);
   };
 
   return (
@@ -103,55 +108,37 @@ const Minter = (props) => {
       <br></br>
       <h1 id="title">🧙‍♂️ NFTime Minter</h1>
       <p>
-        Simply select the desired watch from the list, add a recipient and then press "Mint."
+        Simply add the address of the recipient, select the desired watch from the list and then press "Mint NFT".
       </p>
-      <Form.Control
-        autoFocus
-        className="mx-3 my-2 w-auto"
-        placeholder="Type to filter..."
-        onChange={handleFilterChange}
-      />
-      <Row xs={1} md={5} className="g-4">
-        {filteredInfo.map((watch, idx) => (
-          <Col key={idx}>
-            <Card>
-              <Card.Img variant="top" src={`https://ipfs.io/ipfs/${watch.image}`} />
-              <Card.Body>
-                <Card.Title>{watch.model}</Card.Title>
-                <Card.Text>
-                  {watch.brand} - {watch.year_of_production}
-                </Card.Text>
-                {/* Add any additional watch details as needed */}
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
       <form>
-        <h2>🖼 Hash of asset: </h2>
-        <input
-          type="text"
-          placeholder="e.g. QmSbF9xbradCfPDeV9y1K8e92CkkJ9rdeqePFrgFSfVp9y"
-          onChange={(event) => setHash(event.target.value)}
-        />
-        <h2>🤔 Name: </h2>
-        <input
-          type="text"
-          placeholder="e.g. My first NFT!"
-          onChange={(event) => setName(event.target.value)}
-        />
-        <h2>✍️ Description: </h2>
-        <input
-          type="text"
-          placeholder="e.g. Even cooler than cryptokitties ;)"
-          onChange={(event) => setDescription(event.target.value)}
-        />
         <h2>Recipient: </h2>
         <input
           type="text"
           placeholder="0x..."
           onChange={(event) => setRecipient(event.target.value)}
         />
+        <Form.Control
+          autoFocus
+          className="mx-3 my-2 w-auto"
+          placeholder="Type to filter..."
+          onChange={handleFilterChange}
+        />
+        <Row xs={1} md={5} className="g-4">
+          {filteredInfo.map((watch, idx) => (
+            <Col key={idx}>
+              <Card className={`bg-${selectedWatch === watch ? "info" : "light"}`} onClick={() => handleCardClick(watch)}>
+                <Card.Img variant="top" src={`https://ipfs.io/ipfs/${watch.image}`} />
+                <Card.Body>
+                  <Card.Title>{watch.model}</Card.Title>
+                  <Card.Text>
+                    {watch.brand} - {watch.year_of_production}
+                  </Card.Text>
+                  {/* Add any additional watch details as needed */}
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </form>
       <button id="mintButton" onClick={onMintPressed}>
         Mint NFT
@@ -162,52 +149,5 @@ const Minter = (props) => {
     </div>
   );
 };
-
-// The forwardRef is important!! Dropdown needs access to the DOM node in order to position the Menu
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-  <a
-    href=""
-    ref={ref}
-    onClick={(e) => {
-      e.preventDefault();
-      onClick(e);
-    }}
-  >
-    {children}
-    &#x25bc;
-  </a>
-));
-
-// forwardRef again here! Dropdown needs access to the DOM of the Menu to measure it
-const CustomMenu = React.forwardRef(
-  ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
-    const [value, setValue] = useState('');
-
-    return (
-      <div
-        ref={ref}
-        style={style}
-        className={className}
-        aria-labelledby={labeledBy}
-      >
-        <Form.Control
-          autoFocus
-          className="mx-3 my-2 w-auto"
-          placeholder="Type to filter..."
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-        />
-        <ul className="list-unstyled">
-          {React.Children.toArray(children).filter(
-            (child) => {
-              const childText = Array.from(child.props.children).join('').toLowerCase();
-              return !value || childText.startsWith(value.toLowerCase());
-            }
-          )}
-        </ul>
-      </div>
-    );
-  },
-);
 
 export default Minter;
